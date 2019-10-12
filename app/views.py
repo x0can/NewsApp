b30c06a6df60e099e648f32bc9bd48769d3dcb2f
alexmwaura <alexmwaura43@gmail.com>
2019-10-12 23:27:55 +0300

    add render_template,root page function

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+from .request import get_news
+
+
+@app.route('/')
+def index():
+
+    """
+    view root function that returns the index page and its data
+    """
+    popular_news = get_news("popular")
+    print(popular_news)
+    title = "Home - Welcome to The best News website Online"
+    return render_template("index.html",title = title, popular = popular_news)
+
+@app.route("/news/<int:news_id>")
+def news(news_id):
+    """
+    View news function that returns the news details page and its data
+    """    
+    return render_template("news.html",id = news_id)
