451ca316d7dfe4e956f3b62149954bffc3e9fc47
alexmwaura <alexmwaura43@gmail.com>
2019-10-13 00:43:33 +0300

    modified file

--- app/views.py
+++ app/views.py
@@ -10,9 +10,10 @@ def index():
     view root function that returns the index page and its data
     """
     popular_news = get_news("popular")
-    print(popular_news)
+    upcoming_news = get_news("upcoming")
+    now_trending = get_news("trending")
     title = "Home - Welcome to The best News website Online"
-    return render_template("index.html",title = title, popular = popular_news)
+    return render_template("index.html",title = title, popular = popular_news,upcoming = upcoming_news,trending = now_trending)
 
 @app.route("/news/<int:news_id>")
 def news(news_id):
