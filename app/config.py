09667080fcc46d31c2213bd58c9e6865c64dcd03
alexmwaura <alexmwaura43@gmail.com>
2019-10-13 22:29:58 +0300

    to add article url base and source url base

--- app/config.py
+++ app/config.py
@@ -1,19 +1,29 @@
 class Config:
-    """
-    Genral configuration parent class
-    """
-    NEWS_API_BASE_URL = "https://newsapi.org/v2/everything?q=news&apiKey=e58b87334b40467082f5b07a1192007b"
+    '''
+    General configuration parent class
+    '''
+    NEWS_SOURCE_API_BASE_URL = "https://newsapi.org/v2/sources?apiKey=913ddb20854a426ab84266c7a0de5438"
     
+    NEWS_ARTICLE_BASE_URL = "https://newsapi.org/v2/everything?q=news&apiKey=913ddb20854a426ab84266c7a0de5438"
+
+
 
 class ProdConfig(Config):
-    """
-    Production configuration child class
-    """
+    '''
+    Production  configuration child class
 
-class DevConfig(Config):
-    """
-    Development configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
-    """
-    DEBUG = True        
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+  
+    DEBUG = True 
