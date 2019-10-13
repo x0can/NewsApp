9af31daeeeb013e224cb8968ede3cccc9ce1a4e1
alexmwaura <alexmwaura43@gmail.com>
2019-10-13 22:28:10 +0300

    to update file

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 
-
 app = Flask(__name__,instance_relative_config= True)
+
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
 
-bootstarp = Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 from app import views
+
 
