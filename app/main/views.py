31a1ca88510032d316b5bbfba56ccd21039e7a74
alexmwaura <alexmwaura43@gmail.com>
2019-10-14 16:13:39 +0300

    add function for searching article

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from .import main
-from ..request import get_sources,get_articles
+from ..request import get_sources,get_articles,search_news
+
 
 from ..models import Source
 
@@ -12,7 +13,16 @@ def index():
     """
     popular_news = get_sources("popular")
     title = "Welcome to the best news outlet"
-    return render_template("index.html",title = title,popular = popular_news) 
+
+    search_news = request.args.get("article_query")
+
+    if search_news:
+        return redirect(url_for('main.search',news_feed = search_news))
+
+    else:
+
+        
+        return render_template("index.html",title = title,popular = popular_news) 
 
 
 @main.route("/sources/<sources_id>")
@@ -25,3 +35,28 @@ def sources(sources_id):
     title = f"{sources_id} | All Articles"
     return render_template("news.html",name = sources_id,title = title,news = news_source)
 
+@main.route("/search/<news_feed>")
+def search(news_feed):
+    '''
+    View function to display the search results
+    '''
+
+    news_name_list = news_feed.split(" ")
+    news_name_format = "+".join(news_name_list)
+    
+    searched_news = search_news(news_name_format)
+    title = "News results"
+    #search_news = request.args.get('article_query')
+
+    return render_template("search.html",article = searched_news)
+
+    
+
+
+       
+
+  
+
+    
+
+            
